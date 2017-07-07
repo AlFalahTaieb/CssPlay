d426b14b7a87a10d0960fc446fd9ce1f3bd3beed
al falah taieb <mohamedtaieb.alfalah@esprit.tn>
2017-07-07 21:44:18 +0200

    finishing

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 const input=document.querySelectorAll('.controls input');
+
+function handleU(){
+
+ const suffix=this.dataset.sizing || '';
+ // console.log(this.name);
+ document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
+
+}
+
+input.forEach(input=>input.addEventListener('change',handleU));
+
+input.forEach(input=>input.addEventListener('mousemove',handleU));
